6cec054190351b33cdf45b0bb0d515f504a1c555
newbe01 <pkwaa3@gmail.com>
2024-01-05 16:50:22 +0900

    springdatajpa - domainclassConverter
    pk값은 자동으로 컨버팅해서 객체를 바로 사용할 수 있다

---
+++ src/main/java/spring/datajpa/controller/MemberController.java
@@ -0,0 +1,32 @@
+package spring.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import spring.datajpa.entity.Member;
+import spring.datajpa.repository.MemberRepository;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
