31d5fa810897d53107055c131b01bd25b9b7ff01
newbe01 <pkwaa3@gmail.com>
2024-01-08 11:56:33 +0900

    springdatajpa - specifications

---
+++ src/main/java/spring/datajpa/repository/MemberSpec.java
@@ -0,0 +1,27 @@
+package spring.datajpa.repository;
+
+import jakarta.persistence.criteria.*;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import spring.datajpa.entity.Member;
+import spring.datajpa.entity.Team;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName) {
+        return (root, query, criteriaBuilder) -> {
+
+            if (!StringUtils.hasText(teamName)) {
+                return null;
+            }
+
+            Join<Member, Team> t = root.join("team", JoinType.INNER);
+            return criteriaBuilder.equal(t.get("name"), teamName);
+        };
+    }
+
+    public static Specification<Member> username(final String username) {
+        return (root, query, criteriaBuilder) ->
+            criteriaBuilder.equal(root.get("username"), username);
+    }
+}
--- src/test/java/spring/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/spring/datajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import spring.datajpa.entity.Member;
@@ -328,6 +329,27 @@ class MemberRepositoryTest {
     @Test
     void callCustom() {
         List<Member> memberCustom = repository.findMemberCustom();
+
+    }
+
+    @Test
+    void specBase() {
+
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
+        List<Member> result = repository.findAll(spec);
+
+        assertThat(result.size()).isEqualTo(1);
 
     }
 }
